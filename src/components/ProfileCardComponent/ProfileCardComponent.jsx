b22b43f7b4aa967a1452a6c63139d6b0bb8ab22e
Luisa Giraldo <luiisagiraldori@gmail.com>
2023-10-03 22:33:06 -0500

    Add image top of card and avatar

--- src/components/ProfileCardComponent/ProfileCardComponent.jsx
+++ src/components/ProfileCardComponent/ProfileCardComponent.jsx
@@ -1,9 +1,26 @@
 import "./ProfileCardComponent.scss";
+import imageTop from "../../assets/bg-pattern-card.svg";
+import imageAvatar from "../../assets/image-victor.jpg";
 
 function ProfileCardComponent() {
   return (
     <>
-      <div className="container"></div>
+      <div className="container">
+        <div>
+          <img src={imageTop} />
+          <img src={imageAvatar} />
+        </div>
+        <div className="container-name">
+          <div className="container-name--info-user">
+            <span>
+              <b>Victor Crest</b>
+            </span>
+            <span className="container-name--data">26</span>
+          </div>
+          <div className="container-name--data">London</div>
+        </div>
+        <div className="container-footer"></div>
+      </div>
     </>
   );
 }
