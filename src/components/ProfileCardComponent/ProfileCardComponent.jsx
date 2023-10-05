f8391cff8781279a2425a7014661388014de642b
Luisa Giraldo <luiisagiraldori@gmail.com>
2023-10-04 19:46:10 -0500

    Add info of footer

--- src/components/ProfileCardComponent/ProfileCardComponent.jsx
+++ src/components/ProfileCardComponent/ProfileCardComponent.jsx
@@ -19,7 +19,26 @@ function ProfileCardComponent() {
           </div>
           <div className="container-name--data">London</div>
         </div>
-        <div className="container-footer"></div>
+        <div className="container-footer">
+          <div className="container-footer--item">
+            <span>
+              <b>80K</b>
+            </span>
+            <span className="container-name--data">Followers</span>
+          </div>
+          <div className="container-footer--item">
+            <span>
+              <b>803K</b>
+            </span>
+            <span className="container-name--data">Likes</span>
+          </div>
+          <div className="container-footer--item">
+            <span>
+              <b>1.4K</b>
+            </span>
+            <span className="container-name--data">Photos</span>
+          </div>
+        </div>
       </div>
     </>
   );
